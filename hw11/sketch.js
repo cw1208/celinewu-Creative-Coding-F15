979507fac964cab81d648d76e3b028ce893fa421
Celine W <cw1208@nyu.edu>
2015-12-17 16:24:18 -0500

    Added tree code and images

--- hw11/sketch.js
+++ hw11/sketch.js
@@ -1,4 +1,12 @@
-//code by:
+/* Celine Wu
+Creative Coding, Fall 2015
+Homework 6
+Use a spritesheet to create a small animation
+Fixed mouseClicked behavior, added tree obstacle that disappears on click.
+CodePens link: (not connecting to p5 library?) http://codepen.io/cw1208/pen/QyNLod
+*/
+
+// original code by:
 // https://github.com/Jared-Sprague
 var runningCat;
 var savanna;
@@ -10,6 +18,7 @@ function setup() {
   createCanvas(windowWidth, 800);
 
   savanna = loadImage('image/african-savanna.png');
+  tree = loadImage('image/transpTree.png');
   runningCat = loadImage('image/running-cat.png');
 }
 
@@ -19,9 +28,14 @@ function draw() {
   clear();
   image(savanna,0,0,savanna.width,savanna.height,
   0,0,width,savanna.height);
-
-  // Animate running cat
   
+  // Draw tree in cat's way
+  if(isRunning==false){
+    image(tree,0,0,tree.width,tree.height,
+  325,0,250,250);
+  } 
+  
+  // Animate running cat
   sX += 300;
   if (sX > 300) {
     sX = 0;
@@ -42,7 +56,7 @@ function draw() {
     image(runningCat, sX, sY, 300, 150, 170, 100, 300, 150);
   }
   // Show full sprite sheet for reference
-  image(runningCat, 0, 300, 300, 300);
+  // image(runningCat, 0, 300, 300, 300);
 }
 function runToDest(){
   dX +=5.0;
@@ -51,6 +65,6 @@ function runToDest(){
 
 function mouseClicked(){
   isRunning = true;
-  dest= createVector(mouseX,mouseY);
+  dest= createVector(width,height);
   println(dest.x);
 }
